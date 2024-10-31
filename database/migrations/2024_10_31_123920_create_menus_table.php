c9fb4278d24cb64d6b4bc89d5fe73c36b2c6211c
SimpleMan2022 <aditn689@gmail.com>
2024-10-31 20:07:43 +0700

    feat: make some migration and seeder for starter

---
+++ database/migrations/2024_10_31_123920_create_menus_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('menus', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('parent_id')->nullable()->references('id')->on('menus')->nullOnDelete();
+            $table->string('name');
+            $table->string('slug');
+            $table->string('title');
+            $table->integer('order');
+            $table->string('url')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('menus');
+    }
+};
---
+++ database/migrations/2024_10_31_124137_create_infographics_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('infographics', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->references('id')->on('users')->cascadeOnDelete();
+            $table->string('title');
+            $table->string('image');
+            $table->integer('total_likes')->default(0);
+            $table->integer('total_views')->default(0);
+            $table->integer('total_downloads')->default(0);
+            $table->enum('status', ['pending', 'approved', 'rejected'])->default('pending');
+            $table->timestamp('published_at')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('infographics');
+    }
+};
