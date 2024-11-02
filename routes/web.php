1bbb6c2bd9bfccefe2fc9cfff162828878d810df
SimpleMan2022 <aditn689@gmail.com>
2024-11-02 20:48:05 +0700

    feat: add crud for menu

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,22 @@
 <?php
 
+use App\Http\Controllers\MenuController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', fn() => view('landing.home'));
 
-Route::get('/dashboard', fn() => view('dashboard.home'))->name('dashboard');
-Route::get('/blank', fn() => view('dashboard.blank'))->name('dashboard');
-Route::get('/infographic', fn() => view('dashboard.infographic'))->name('dashboard');
+Route::prefix('dashboard')->group(function () {
+  Route::get('/', fn() => view('dashboard.home'))->name('dashboard');
+  Route::get('/blank', fn() => view('dashboard.blank'))->name('dashboard');
+  Route::get('/infographic', fn() => view('dashboard.infographic'))->name('dashboard');
+
+  Route::resource('menus', MenuController::class)->names([
+    'index' => 'menus.index',
+    'create' => 'menus.create',
+    'store' => 'menus.store',
+    'show' => 'menus.show',
+    'edit' => 'menus.edit',
+    'update' => 'menus.update',
+    'destroy' => 'menus.destroy',
+  ]);
+});
