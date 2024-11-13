e2e3ce5413d62b0853e470544f58bb57d447b183
adilasomaaa <yasdilofficial@gmail.com>
2024-11-13 16:07:28 +0800

    feat(menu):fix pengelompokkan

--- resources/views/partials/dashboard/sidebar.blade.php
+++ resources/views/partials/dashboard/sidebar.blade.php
@@ -17,36 +17,72 @@
         </li>
 
         @if (auth()->user()->role === 'superadmin')
-            <li class="sidebar-item {{ Request::route()->named('menus.*') ? 'active' : '' }}">
-                <a href="{{ route('menus.index') }}" class="sidebar-link">
-                    <i class="bi bi-list-ul"></i>
-                    <span>Menu</span>
-                </a>
-            </li>
-
-            <li class="sidebar-item {{ Request::route()->named('subjects.*') ? 'active' : '' }}">
-                <a href="{{ route('subjects.index') }}" class="sidebar-link">
-                    <i class="bi bi-journal-bookmark-fill"></i>
-                    <span>Subjek</span>
-                </a>
-            </li>
-
-            <li class="sidebar-item {{ Request::route()->named('indicators.*') ? 'active' : '' }}">
-                <a href="{{ route('indicators.index') }}" class="sidebar-link">
-                    <i class="bi bi-bar-chart-line-fill"></i>
-                    <span>Indikator</span>
-                </a>
-            </li>
-            <li class="sidebar-item {{ Request::route()->named('feedbacks.*') ? 'active' : '' }}">
-                <a href="{{ route('feedbacks.index') }}" class="sidebar-link">
-                    <i class="bi bi-envelope"></i>
-                    <span>Feedback</span>
-                    @if ($totalFeedbacks > 0)
-                        <span class="badge bg-danger">{{ $totalFeedbacks }}</span>
-                    @endif
-                </a>
-            </li>
-
+        <li class="sidebar-item  has-sub">
+            <a href="#" class="sidebar-link">
+                <i class="bi bi-file-earmark-spreadsheet-fill"></i>
+                <span>Master Data</span>
+            </a>
+            
+            <ul class="submenu submenu-open" style="--submenu-height: 86px;">                
+                
+                <li class="submenu-item {{ Request::route()->named('menus.*') ? 'active' : '' }}">
+                    <a href="{{ route('menus.index') }}" class="submenu-link">
+                        {{-- <i class="bi bi-list-ul"></i> --}}
+                        <span>Menu</span>
+                    </a>
+                </li>
+    
+                <li class="submenu-item {{ Request::route()->named('subjects.*') ? 'active' : '' }}">
+                    <a href="{{ route('subjects.index') }}" class="submenu-link">
+                        {{-- <i class="bi bi-journal-bookmark-fill"></i> --}}
+                        <span>Subjek</span>
+                    </a>
+                </li>
+    
+                <li class="submenu-item {{ Request::route()->named('indicators.*') ? 'active' : '' }}">
+                    <a href="{{ route('indicators.index') }}" class="submenu-link">
+                        {{-- <i class="bi bi-bar-chart-line-fill"></i> --}}
+                        <span>Indikator</span>
+                    </a>
+                </li>                
+            </ul>            
+        </li>
+        <li class="sidebar-item  has-sub">
+            <a href="#" class="sidebar-link">
+                <i class="bi bi-file-earmark-spreadsheet-fill"></i>
+                <span>Transaksi</span>
+            </a>
+            
+            <ul class="submenu submenu-open" style="--submenu-height: 86px;">                
+                
+                <li class="submenu-item {{ Request::route()->named('infographics.*') ? 'active' : '' }}">
+                    <a href="{{ route('infographics.index') }}" class="submenu-link">
+                        
+                        <span>Infographics</span>
+                    </a>
+                </li>
+        
+                <li class="submenu-item {{ Request::route()->named('videos.*') ? 'active' : '' }}">
+                    <a href="{{ route('videos.index') }}" class="submenu-link">
+                        <span>Video</span>
+                    </a>
+                </li>
+    
+                <li class="submenu-item {{ Request::route()->named('documents.*') ? 'active' : '' }}">
+                    <a href="{{ route('documents.index') }}" class="submenu-link">
+                        <span>Document</span>
+                    </a>
+                </li>
+    
+                <li class="submenu-item {{ Request::route()->named('indicators.*') ? 'active' : '' }}">
+                    <a href="{{ route('indicators.index') }}" class="submenu-link">
+                        {{-- <i class="bi bi-bar-chart-line-fill"></i> --}}
+                        <span>Indikator</span>
+                    </a>
+                </li>                
+            </ul>            
+        </li>
+                        
 
             <li class="sidebar-item {{ Request::route()->named('users.*') ? 'active' : '' }}">
                 <a href="{{ route('users.index') }}" class="sidebar-link">
@@ -56,26 +92,9 @@
             </li>
         @endif
 
-        <li class="sidebar-item {{ Request::route()->named('documents.*') ? 'active' : '' }}">
-            <a href="{{ route('documents.index') }}" class="sidebar-link">
-                <i class="bi bi-file-earmark-text-fill"></i>
-                <span>Document</span>
-            </a>
-        </li>
+       
 
-        <li class="sidebar-item {{ Request::route()->named('infographics.*') ? 'active' : '' }}">
-            <a href="{{ route('infographics.index') }}" class="sidebar-link">
-                <i class="bi bi-images"></i>
-                <span>Infographics</span>
-            </a>
-        </li>
-
-        <li class="sidebar-item {{ Request::route()->named('videos.*') ? 'active' : '' }}">
-            <a href="{{ route('videos.index') }}" class="sidebar-link">
-                <i class="bi bi-play-btn-fill"></i>
-                <span>Video</span>
-            </a>
-        </li>
+       
 
         <li class="sidebar-item">
             <a href="{{ route('logout') }}" class="sidebar-link"
